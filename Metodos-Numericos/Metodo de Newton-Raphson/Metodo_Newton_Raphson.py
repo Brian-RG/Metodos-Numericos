1191afa7e709efc61fa554a666cd1302a762fab3
Brian R <brianreyesg@hotmail.com>
2018-11-05 12:20:51 -0600

    Update Metodo_Newton_Raphson.py

--- Metodos-Numericos/Metodo de Newton-Raphson/Metodo_Newton_Raphson.py
+++ Metodos-Numericos/Metodo de Newton-Raphson/Metodo_Newton_Raphson.py
@@ -1,17 +1,64 @@
-#f'(x0)= f(x0)/x0-xr
-#x0-xr=f(x0)/f'(x0)
-#xr=x0-f(x0)/f'(x0)
-def g(x):
-    return (x**4)-(8.6*(x**3))-(35.51*(x**2))+(464*x)-998.46
-
-def gprima(x):
-    return (4*(x**3))-(25.8*(x**2))-(71.02*x)+464
-
-def Newton(x0):
-    for e in range(100):
-        xr=x0-(g(x0)/gprima(x0))
-        if(abs(g(xr))<0.0001):
-            print("La raíz es " + str(xr))
-            break
-        x0=xr
-Newton(7)
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {
+    "collapsed": false
+   },
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Usando de rango 0 y 1\n",
+      "La raíz es 0.585693359375\n",
+      "Usando de rango 1 y 3.2\n",
+      "La raíz es 3.0001953125000007\n",
+      "Usando de rango 3.2 y 4\n",
+      "La raíz es 3.4140625000000004\n"
+     ]
+    }
+   ],
+   "source": [
+    "#f'(x0)= f(x0)/x0-xr\n",
+    "#x0-xr=f(x0)/f'(x0)\n",
+    "#xr=x0-f(x0)/f'(x0)\n",
+    "def g(x):\n",
+    "    return (x**4)-(8.6*(x**3))-(35.51*(x**2))+(464*x)-998.46\n",
+    "\n",
+    "def gprima(x):\n",
+    "    return (4*(x**3))-(25.8*(x**2))-(71.02*x)+464\n",
+    "\n",
+    "def Newton(x0):\n",
+    "    for e in range(100):\n",
+    "        xr=x0-(g(x0)/gprima(x0))\n",
+    "        if(abs(g(xr))<0.0001):\n",
+    "            print(\"La raíz es \" + str(xr))\n",
+    "            break\n",
+    "        x0=xr\n",
+    "Newton(7)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 0,
+   "metadata": {
+    "collapsed": false
+   },
+   "outputs": [
+   ],
+   "source": [
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (Ubuntu Linux)",
+   "language": "python",
+   "name": "python3"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 0
+}
