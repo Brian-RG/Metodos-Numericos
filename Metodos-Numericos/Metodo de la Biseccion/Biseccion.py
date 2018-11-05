34b0dc6530605e8694bac34c3b86dc6e90c60916
Brian R <brianreyesg@hotmail.com>
2018-11-05 12:13:38 -0600

    Update Biseccion.py

--- Metodos-Numericos/Metodo de la Biseccion/Biseccion.py
+++ Metodos-Numericos/Metodo de la Biseccion/Biseccion.py
@@ -1,32 +1,79 @@
-import math
-def f(x):
-    return (x**3)-(7*(x**2))+(14*x)-6
-#probar con 3 rangos x0,x1 ; y0,y1; z0,z1
-
-def Biseccion(x0,x1):
-    print("Usando de rango " +str(x0) + " y " +str(x1))
-    for i in range(100):
-        fx0=f(x0)
-        fx1=f(x1)
-        if(fx0*fx1>0):
-            #Checa, si la multiplicación da arriba de 0 es porque o ambos son positivos, o ambos son negativos, entonces en ningún momento         te da la raíz
-            print("No hay raíz en el rango dado")
-            break
-        xr=(x1+x0)/2
-        fxr=f(xr)
-        if(fxr*fx1>0):
-            x1=xr
-        elif(fxr*fx0>0):
-            x0=xr
-        if(abs(fxr)<0.001):
-            print("La raíz es "+str(xr))
-            break
-x0=0
-x1=1
-y0=1
-y1=3.2
-z0=3.2
-z1=4
-Biseccion(x0,x1)
-Biseccion(y0,y1)
-Biseccion(z0,z1)
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {
+    "collapsed": false
+   },
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Usando de rango 0 y 1\n",
+      "La raíz es 0.585693359375\n",
+      "Usando de rango 1 y 3.2\n",
+      "La raíz es 3.0001953125000007\n",
+      "Usando de rango 3.2 y 4\n",
+      "La raíz es 3.4140625000000004\n"
+     ]
+    }
+   ],
+   "source": [
+    "import math\n",
+    "def f(x):\n",
+    "    return (x**3)-(7*(x**2))+(14*x)-6\n",
+    "#probar con 3 rangos x0,x1 ; y0,y1; z0,z1\n",
+    "\n",
+    "def Biseccion(x0,x1):\n",
+    "    print(\"Usando de rango \" +str(x0) + \" y \" +str(x1))\n",
+    "    for i in range(100):\n",
+    "        fx0=f(x0)\n",
+    "        fx1=f(x1)\n",
+    "        if(fx0*fx1>0):\n",
+    "            #Checa, si la multiplicación da arriba de 0 es porque o ambos son positivos, o ambos son negativos, entonces en ningún momento         te da la raíz\n",
+    "            print(\"No hay raíz en el rango dado\")\n",
+    "            break\n",
+    "        xr=(x1+x0)/2\n",
+    "        fxr=f(xr)\n",
+    "        if(fxr*fx1>0):\n",
+    "            x1=xr\n",
+    "        elif(fxr*fx0>0):\n",
+    "            x0=xr\n",
+    "        if(abs(fxr)<0.001):\n",
+    "            print(\"La raíz es \"+str(xr))\n",
+    "            break\n",
+    "x0=0\n",
+    "x1=1\n",
+    "y0=1\n",
+    "y1=3.2\n",
+    "z0=3.2\n",
+    "z1=4\n",
+    "Biseccion(x0,x1)\n",
+    "Biseccion(y0,y1)\n",
+    "Biseccion(z0,z1)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 0,
+   "metadata": {
+    "collapsed": false
+   },
+   "outputs": [
+   ],
+   "source": [
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (Ubuntu Linux)",
+   "language": "python",
+   "name": "python3"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 0
+}
